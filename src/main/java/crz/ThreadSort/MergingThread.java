3b6ea1336ddb627d994e43b4cc0338fd777a6dfb
Leonardo Davalos <32350741+leothecrz@users.noreply.github.com>
2024-02-21 01:11:08 -0800

    started merge thread 2

--- src/main/java/crz/ThreadSort/MergingThread.java
+++ src/main/java/crz/ThreadSort/MergingThread.java
@@ -1,5 +1,6 @@
 package crz.ThreadSort;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
@@ -8,23 +9,44 @@ class MergingThread implements Runnable
 
     private Integer[] toSortArray;
     private int[] startIndexes;
+    private int[] indexPointers;
+    private int partitionSize;
 
-    private PriorityQueue queue;
+    private PriorityQueue<Integer> queue;
 
     public MergingThread(Integer[] ref, Integer[] sortRef, int[] starts)
     {
         toSortArray = ref;
         startIndexes = starts;
 
-        queue = new PriorityQueue<>(Comparator.naturalOrder());
-        
+        indexPointers = Arrays.copyOf(starts, starts.length);
 
+        if(starts.length > 1)
+            partitionSize = starts[1] - starts[0];
+        else
+            partitionSize = ref.length;
+
+        queue = new PriorityQueue<>(Comparator.naturalOrder());
     }
 
     // TODO:: Given n SORTED partitions and the start index of each partion, merge into a single sorted list.
     @Override
     public void run()
-    {
+    {        
+
+        int i = 0;
+
+        for (int j = 0; j < startIndexes.length; j++) 
+        {
+            queue.add(toSortArray[startIndexes[i]]);
+            
+        }
+
+        while (!queue.isEmpty()) 
+        {
+            
+        }
+
 
     }
 
